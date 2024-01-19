183de6b59ebcf6a3c453f6201ba57675869446d5
FarizaMukh <145853732+FarizaMukh@users.noreply.github.com>
2024-01-18 20:21:45 -0600

    hhh

--- backend.tf
+++
@@ -1,7 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket = "freezybucket"
-    key    = "path/to/my/key"
-    region = "us-east-2"
-  }
-}
--- vpc.tf
+++ vpc.tf
@@ -1,8 +1,1 @@
-resource "aws_vpc" "Freezy" {
-  cidr_block       = "10.0.0.0/16"
-  instance_tenancy = "default"
 
-  tags = {
-    Name = "main"
-  }
-}
