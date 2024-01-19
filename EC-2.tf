25f3c18bd8836b2b776d69fb69966d401e845905
FarizaMukh <145853732+FarizaMukh@users.noreply.github.com>
2024-01-18 20:12:59 -0600

    Update EC-2.tf

--- EC-2.tf
+++ EC-2.tf
@@ -1,8 +1,8 @@
-#esource "aws_instance" "test" {
- # ami           = "ami-0cd3c7f72edd5b06d"
- # instance_type = "t2.micro"
+resource "aws_instance" "test" {
+  ami           = "ami-0cd3c7f72edd5b06d"
+  instance_type = "t2.micro"
 
- # tags = {
-#  Name = "FreezyExample"
-#  }
-#}
+  tags = {
+  Name = "FreezyExample"
+  }
+}
